034929cbe1bfb37124fdb19759b9e4c63c0d95fe
Shreyans18-art <24bma036@nith.ac.in>
2026-03-22 18:59:08 +0530

    testing auto merge again

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Final test" << std::endl;
+    std::cout << "Final test 2" << std::endl;
     return 0;
 }
--- my-bot/bots/ciBot.js
+++ my-bot/bots/ciBot.js
@@ -1,27 +1,33 @@
 export default (app) => {
 
-  app.on("check_run.completed", async (context) => {
+  app.on("check_suite.completed", async (context) => {
 
-    const conclusion = context.payload.check_run.conclusion;
+    const conclusion = context.payload.check_suite.conclusion;
 
-    const pr = context.payload.check_run.pull_requests[0];
+    const pr = context.payload.check_suite.pull_requests[0];
     if (!pr) return;
 
     const issue_number = pr.number;
 
-   if (conclusion === "success") {
-  message = "✅ CI Passed! Ready to merge 🚀";
+    let message = "";
 
-  try {
-    await context.octokit.pulls.merge({
-      owner: context.payload.repository.owner.login,
-      repo: context.payload.repository.name,
-      pull_number: issue_number,
-    });
-  } catch (err) {
-    console.log("Auto merge failed:", err.message);
-  }
-}
+    if (conclusion === "success") {
+      message = "✅ CI Passed! Auto-merging PR 🚀";
+
+      try {
+        await context.octokit.pulls.merge({
+          owner: context.payload.repository.owner.login,
+          repo: context.payload.repository.name,
+          pull_number: issue_number,
+        });
+      } catch (err) {
+        console.log("Auto merge failed:", err.message);
+      }
+
+    } else {
+      message = "❌ CI Failed! Fix your code 😤";
+    }
+
     await context.octokit.issues.createComment({
       owner: context.payload.repository.owner.login,
       repo: context.payload.repository.name,
